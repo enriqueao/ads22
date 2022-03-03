c17cd070f39cf63445479dcb69a153172ad77772
Ricardo Ángeles <ricardohead@gmail.com>
2022-03-02 21:54:25 -0600

    First commit

---
+++ hw/rdaf-simpleapp/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('hola desde express')
+});
+
+app.listen(port, () => console.log('running en el puerto 3000'));
